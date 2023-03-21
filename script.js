e2a89b9a816de572881055739e3248d822091efe
al3xs1lv4 <alessandro.gomes.free@gmail.com>
2023-03-21 08:56:32 -0300

    Adicionando bairro no preenchimento automático, e readonly nas informações

--- script.js
+++ script.js
@@ -19,13 +19,16 @@ async function buscaEndereco(cep) {
         if (consultaCEPConvertida.erro) {
             throw Error('CEP não existente!');
         }
+        console.log(consultaCEPConvertida);
         var cidade = document.getElementById('cidade');
         var logradouro = document.getElementById('endereco');
         var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
 
         cidade.value = consultaCEPConvertida.localidade;
         logradouro.value = consultaCEPConvertida.logradouro;
         estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
         return consultaCEPConvertida;
     } catch (erro) {
         mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`
